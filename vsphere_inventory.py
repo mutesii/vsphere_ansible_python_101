c74687d56e8af048d66f3a25d1b020606c721e10
Sofi <40280900+mutesii@users.noreply.github.com>
2019-02-07 14:19:36 -0800

    Update vsphere_inventory.py

--- vsphere_inventory.py
+++ vsphere_inventory.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python
-
-#   This file is part of vSphere Ansible Inventory.
-#
-#   vSphere Ansible Inventory is free software: you can redistribute it and/or
-#   modify it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 2 of the License, or (at your
-#   option) any later version.
-#
-#   vSphere Ansible Inventory is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#   GNU General Public License for more details.
-#   You should have received a copy of the GNU General Public License
-#   along with vSphere Ansible Inventory. If not, see:
-#   <http://www.gnu.org/licenses/>.
-#
-# Authors:
-# version 0.1 Michael-Angelos Simos
-
 """
 Python script for listing VMware vSphere Virtual Machines for Ansible Inventory
 """
